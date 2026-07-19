da0085ccf643d0d335f9a13ac75f1eb7029ca733
PriyaDharshini22-2005 <158809809+PriyaDharshini22-2005@users.noreply.github.com>
2026-07-19 10:07:49 +0530

    asgi.py

--- travelai/travelai/asgi.py
+++ travelai/travelai/asgi.py
@@ -9,5 +9,4 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelai.settings")
-
 application = get_asgi_application()
